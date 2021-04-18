9ab973c6aa414173705d5bb1061de50558f1d654
merikson <merikson1@hotmail.com>
2021-04-18 09:41:22 -0300

    Alterada a tela inicial e centralizadas as outras telas, exceto a de consultas ela continuará sobrescrevendo devido ao tamanho.

--- cadastroentrada.php
+++ cadastroentrada.php
@@ -5,57 +5,61 @@
 		<title>Cadastro de Visitantes</title>
 	</head>
 	<body>
-		<form action="insert.php" method="post">
-			<table>
-				<tr><td colspan="2">Cadastro de Visitantes</td>
-				</tr>
-				<tr>
-					<td>Nome:</td>
-					<td><input type="text" name="nome" required ></td>
-				</tr>
-				<tr>
-					<td>CPF / CNPJ:</td>
-					<td><input type="text" maxlength=20 name="cpfcnpj" required ></td>
-				</tr>
-				<tr>
-					<td>Identidade:</td>
-					<td><input type="text" name="rg" required ></td>
-				</tr>
-				<tr>
-					<td>Destino:</td>
-					<td><input type="text" name="destino" required ></td>
-				</tr>
-				<tr>
-					<td>Veículo:</td>
-					<td><input type="text" name="veiculo"></td>
-				</tr>
-				<tr>
-					<td>Placa:</td>
-					<td><input type="text" name="placa"></td>
-				</tr>
-				<tr>
-					<td>Empresa:</td>
-					<td><input type="text" name="empresa" required ></td>
-				</tr>
-				<tr>
-					<td>Tipo:</td>
-					<td>
-						<select name="tipo" id="tipo">    
-							<option value="" selected></option>
-							<option value="Visitante">Visitante</option>
-							<option value="Prest.Servico">Prest.Servico</option>
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<td>Observação:</td>
-					<td><textarea name="observacao" cols="30" rows="5"></textarea></td>
-				</tr>
-			</table>
-			<br><br>
-			<input type="submit" name="Enviar">
-			<input type="button" value="Voltar" onClick="history.go(-1)">
-			<input type="button" value="Atualizar" onClick="history.go(-0)">
-		</form>
+		<div class="container" >
+			<div class="content">  
+				<form action="insert.php" method="post">
+					<table>
+						<tr></br><td colspan="2">Cadastro de Visitantes</td>
+						</tr>
+						<tr>
+						<td>Nome:</td>
+							<td><input type="text" name="nome" required size = 50></td>
+						</tr>
+						<tr>
+							<td>CPF / CNPJ:</td>
+							<td><input type="text" maxlength=20 name="cpfcnpj" required size = 50></td>
+						</tr>
+						<tr>
+							<td>Identidade:</td>
+							<td><input type="text" name="rg" required size = 50></td>
+						</tr>
+						<tr>
+							<td>Destino:</td>
+							<td><input type="text" name="destino" required size = 50></td>
+						</tr>
+						<tr>
+							<td>Veículo:</td>
+							<td><input type="text" name="veiculo" size = 50></td>
+						</tr>
+						<tr>
+							<td>Placa:</td>
+							<td><input type="text" name="placa" size = 50></td>
+						</tr>
+						<tr>
+							<td>Empresa:</td>
+							<td><input type="text" name="empresa" required size = 50 ></td>
+						</tr>
+						<tr>
+							<td>Tipo:</td>
+							<td>
+								<select name="tipo" id="tipo">    
+									<option value="" selected></option>
+									<option value="Prestador de Servico">Prestador de Servico</option>
+									<option value="Visitante">Visitante</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>Observação:</td>
+							<td><textarea name="observacao" cols="50" rows="5"></textarea></td>
+						</tr>
+					</table>
+					<br>
+					<input type="submit" name="Enviar">
+					<input type="button" value="Voltar" onClick="history.go(-1)">
+					<input type="button" value="Atualizar" onClick="history.go(-0)">
+				</form>
+			</div>
+		</div>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- Alterado para exibir tela inicial
 <!DOCTYPE html>
 <html>
 	<head>
@@ -12,5 +13,43 @@
 			</ul>
 		</nav>
 		<br><br>
+	</body>
+</html>
+-->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>CONTROLE DE ACESSO - Página Inicial</title>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+	<body>
+		<div class="container" >
+			<div class="content">      
+				<!--FORMULÁRIO DE LOGIN-->
+				<div id="login">
+					<form method="post" action="index2.php"> 
+						<h1>Controle de Acesso</h1> 
+						<!--em desenvolvimento
+						<p> 
+							<label for="usuario">Usuário:</label>
+							<input id="usuario" name="usuario" required="required" type="text" placeholder="Digite seu usuário"/>
+						</p>
+						<p> 
+							<label for="senha">Senha:</label>
+							<input id="senha" name="senha" required="required" type="password" placeholder="Digite sua senha" /> 
+						</p>
+						-->
+						</br></br></br></br>
+						<p> 
+							<input type="submit" value="Conectar" /> 
+						</p>
+						<!--<p class="link">
+							<a href="cadastrousuario.php">Cadastrar novo usuário</a>-->
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>  
 	</body>
 </html>
